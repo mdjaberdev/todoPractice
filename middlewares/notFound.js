a501b4b789917bbd113fa2c5175b0adc71d30259
mdjaberdev <mdjaber.dev@gmail.com>
2026-07-11 21:29:40 +0600

    reset

--- middlewares/notFound.js
+++
@@ -1,8 +0,0 @@
-const notFound = (req, res, next) => {
-  res.status(404).json({
-    success: false,
-    message: `Cannot find ${req.originalUrl} not found on this server`
-  });
-};
-
-module.exports = notFound;
